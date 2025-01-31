346f65108dfe0ecc47468345d4c40336234045fd
Maruf Ahmed Rahad <marufrahad253@gmail.com>
2025-01-31 23:42:26 +0100

    string basics added

---
+++ strings.py
@@ -0,0 +1,17 @@
+course = "Python for Beginners"
+# upper and lower does not change the original string
+print(course.upper())
+print(course.lower())
+print(course)
+# find the first return of the string
+print(course.find("n"))
+# if the string is missing, returns -1
+print(course.find("z"))
+# replace in the entire string
+print(course.replace("for","4"))
+
+# return true or false based on the word(Python) is existed in the string(course) or not
+print("Python" in course)
+
+
+
