a7990102844a95b95183001c4ac390196d71efbc
Maruf Ahmed Rahad <marufrahad253@gmail.com>
2025-02-01 00:27:19 +0100

    added Arithmetic Operators

---
+++ ArithmeticOperators.py
@@ -0,0 +1,16 @@
+print(5+3)
+print(5 - 2)
+print(5 * 2)
+# use single division ( / ) for floating result
+print(10 / 3)
+# use double division ( // ) for integer result
+print(10 // 3)
+# reminder of the division
+print(10 % 3)
+# ** represents the power, 10 ** 3 means, 10 power 3 = 1000
+print(10 ** 3)
+
+x = 2
+x += 2
+x -= 4
+print(x)
