5151a670b4c99229481091d60226657fd1f2c14f
Maruf Ahmed Rahad <marufrahad253@gmail.com>
2025-02-01 01:32:54 +0100

    added while loops statement

---
+++ WhileLoop.py
@@ -0,0 +1,14 @@
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+i = 1
+while i <= 3:
+    x = int(input())
+    print(x)
+    i = i + 1
+
+i = 1
+while i <= 5:
+    print(i * "*")
+    i = i + 1
